242d692c7fff1285a6e5a3b13722e54b9c133d3c
Sebastien Johnson <sebastien.z.johnson@gmail.com>
2025-12-24 12:39:22 -0500

    Updating splitting markdown solution

--- src/splitting_text/splitting_markdown.py
+++ src/splitting_text/splitting_markdown.py
@@ -1,7 +1,70 @@
 #splitting raw markdown into text nodes
+from src.splitting_text.extract_markdown import extract_markdown_images, extract_markdown_links
+from src.nodes.textnode import TextNode, TextType
 
 def split_nodes_image(old_nodes):
-    
+    new_nodes = []
+    for old_node in old_nodes:
+        #appends and progresses if not a link/image node
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        #grabs alt texts and links from current node
+        images = extract_markdown_images(original_text)
+
+        #appends and progresses if no link/image 
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+
+        i = 0
+        current_image = f"![{images[i][0]}]({images[i][1]}"
+
+        while i <= len(images):
+
+            #update current section to be rest of previous split text
+
+
+            sections = original_text.split(f"![{images[0]}]({images[1]})", 1)
+            #removes if not both alt text and link present in section
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
 
 
 def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
---
+++ src/tests/test_splitting_markdown.py
@@ -0,0 +1,22 @@
+import unittest
+from src.splitting_text.splitting_markdown import split_nodes_image, split_nodes_link
+from src.nodes.textnode import TextNode, TextType
+
+class TestSplittingMarkdown(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
